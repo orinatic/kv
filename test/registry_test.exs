41b27a55788f8cd3e1b30948f47ed5b06b2628f7
Eli Davis <orinatic@gmail.com>
2017-04-30 17:08:02 -0700

    finished ets

--- test/registry_test.exs
+++ test/registry_test.exs
@@ -2,8 +2,8 @@ defmodule KV.RegistryTest do
   use ExUnit.Case, async: true
 
   setup context do
-    {:ok, registry} = KV.Registry.start_link(context.test)
-    {:ok, registry: registry}
+    {:ok, _} = KV.Registry.start_link(context.test)
+    {:ok, registry: context.test}
   end
 
   test "spawn buckets", %{registry: registry} do
@@ -21,6 +21,9 @@ defmodule KV.RegistryTest do
     KV.Registry.create(registry, "shopping")
     {:ok, bucket} = KV.Registry.lookup(registry, "shopping")
     Agent.stop(bucket)
+
+    # Call to make sure the registry processed the DOWN message
+    _ = KV.Registry.create(registry, "bogus")
     assert KV.Registry.lookup(registry, "shopping") == :error
   end
 
@@ -35,6 +38,8 @@ defmodule KV.RegistryTest do
     #Wait until the bucket dies
     assert_receive {:DOWN, ^ref, _, _, _}
 
+    # Do a call to make sure the registry processed the DOWN message
+    _ = KV.Registry.create(registry, "bogus")
     assert KV.Registry.lookup(registry, "shopping") == :error
   end
 end
